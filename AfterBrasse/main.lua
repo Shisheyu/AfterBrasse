8dcd81e3bcbd65506f588cb71266b01d5bdbeddd
Krayz <Krayz.m@gmail.com>
2017-01-19 12:55:30 +0100

    Update main.lua

--- AfterBrasse/main.lua
+++ AfterBrasse/main.lua
@@ -1,5 +1,4 @@
 --[[
--Krayz-
 --]]
 --Mod
 AfterBrasse = RegisterMod("AfterBrasse", 1)
@@ -10,14 +9,14 @@ Items = {
 				brave_shoe_i = Isaac.GetItemIdByName("Brave Shoe");
 }
 
---While waiting for a working "require"... get external source anyway by using include("");
+--[Krayz]While waiting for a working "require"... get external source anyway by using include("");
 local _f = io.open("savedatapath.txt", "r")
 local _data = _f:read("*all"); _f:close()
 local _, _e = string.find(_data, "Modding Data Path: ")
 local _path = string.reverse(string.match(string.reverse(string.sub(_data, _e+1, string.len(_data))), '/.-$'))
 local include = function(file) local _Npath = _path .."/"..AfterBrasse.Name.."/"..file dofile(_Npath) end
 
-include("srcs/test_debug") --comment this line to disable cheats
+include("srcs/test_debug") --[Krayz]comment this line to disable cheats
 include("srcs/items/cricketspaw.lua")
 include("srcs/items/moneyisluck.lua")
 include("srcs/items/beer.lua")
