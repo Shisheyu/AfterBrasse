6d51f6e1bcf2e540de47dc92cce05fde837a1594
Dogeek <simon.bordeyne@gmail.com>
2017-03-17 18:52:35 +0100

    removed luadocs from master

--- LuaDocs/group___functions.js
+++
@@ -1,9 +0,0 @@
-var group___functions =
-[
-    [ "Game", "group___functions.html#gae643f2600404d3a34d6236fec678aa7c", null ],
-    [ "Random", "group___functions.html#gab0914b8a441bd429bdd637e6b31dfd7c", null ],
-    [ "RandomVector", "group___functions.html#ga1c1c3aca8b6e9a035bae2c4604c999b2", null ],
-    [ "ScreenToWorldDistance", "group___functions.html#ga571e105c642fd8b74fbf1bbe077bae6d", null ],
-    [ "WorldToRenderPosition", "group___functions.html#ga7af3a968a47aff7f6bff8f918e463479", null ],
-    [ "WorldToScreenDistance", "group___functions.html#gabf44cc0c778d3fa2bba427fd21585d5a", null ]
-];
